b12d581afab256547c7b44706e773fd585ffdc9f
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-11-12 19:38:15 +0330

    create article and category model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Article,Category
 
-# Register your models here.
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display=('title','slug','author','status','published')
+    list_filter=('published','status')
+    search_fields=('title','description')
+    prepopulated_fields={'slug':('title',)}
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=('title','slug','parent','status')
+    list_filter=(('status',))
+    search_fields=('title','slug')
+    prepopulated_fields={'slug':('title',)}
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+class Category(models.Model):
+	parent=models.ForeignKey('self',default=None,null=True,blank=True,on_delete=models.SET_NULL,related_name='children')
+	title=models.CharField(max_length=200)
+	slug=models.SlugField(max_length=200,unique=True)
+	status=models.BooleanField(default=True)
+
+	class Meta:
+		verbose_name_plural='Categories'
+		ordering=['parent__id']
+	def __str__(self):
+		return self.title
+
+class Article(models.Model):
+	STATUS_CHOICE=[
+		('d','draft'),
+		('p','publish'),
+		('r','reject'),
+		('i','investigate')
+
+	]
+
+	title=models.CharField(max_length=200)
+	slug=models.SlugField(max_length=200,unique=True)
+	author=models.ForeignKey(settings.AUTH_USER_MODEL,null=True,on_delete=models.SET_NULL,related_name='articles')
+	category=models.ManyToManyField(Category,related_name='articles')
+	description=models.TextField()
+	thumbnail=models .ImageField(upload_to='article-image')
+	published=models.DateTimeField(default=timezone.now)
+	created=models.DateTimeField(auto_now_add=True)
+	updated=models.DateTimeField(auto_now=True)
+	status=models.CharField(max_length=1,choices=STATUS_CHOICE)
+
+	def __str__(self):
+		return self.title
+
+	class Meta:
+		ordering=['published']
