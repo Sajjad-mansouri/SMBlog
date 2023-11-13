3d1e3e1606101df5d94d64fc114e4be825dd9001
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-11-13 23:56:13 +0330

    change post list view and retrieve view to viewset

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path,include
-from .views import PostList,PostDetail,CategoryList,CategoryPostList
+from .views import PostApiViewSet,CategoryList,CategoryPostList
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'posts', PostApiViewSet, basename='posts')
 
 urlpatterns = [
-	path("posts/", PostList.as_view(),name='api_post_list'),
-	path("posts/<int:pk>", PostDetail.as_view(),name='api_post_detail'),
 
 	path("categories/", CategoryList.as_view(),name='api_category_list'),
 	path("categories/<int:pk>", CategoryPostList.as_view(),name='api_category_post_list'),
@@ -18,3 +20,5 @@ urlpatterns = [
 	path("jwt/", TokenObtainPairView.as_view(),name="jwt_obtain_pair"),
 	path("jwt/refresh/", TokenRefreshView.as_view(),name="jwt_refresh"),
 ]
+
+urlpatterns+=router.urls
--- api/views.py
+++ api/views.py
@@ -1,29 +1,20 @@
-from rest_framework import generics
+from rest_framework import generics,viewsets
 from .serializers import PostSerializer,CategorySerializer
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers
 from blog.models import Post,Category
 from .permissions import IsAuthorOrReadonly,IsSuperUser
 
 
-class PostList(generics.ListCreateAPIView):
+class PostApiViewSet(viewsets.ModelViewSet):
 	queryset = Post.objects.all()
 	serializer_class = PostSerializer
-	permission_classes=[IsAuthorOrReadonly|IsSuperUser]
 
 	@method_decorator(cache_page(300))
-	def get(self, request, *args, **kwargs):
-		return super().get(request,*args,**kwargs)
-
-
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = Post.objects.all()
-	serializer_class = PostSerializer
-	permission_classes=[IsAuthorOrReadonly|IsSuperUser]
-
-	@method_decorator(cache_page(300))
-	def get(self, request, *args, **kwargs):
-		return super().get(request,*args,**kwargs)
+	@method_decorator(vary_on_headers("Authorization",'Cookie'))
+	def list(self, request, *args, **kwargs):
+		return super().list(request,*args,**kwargs)
 
 
 class CategoryList(generics.ListCreateAPIView):
