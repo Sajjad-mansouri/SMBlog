af359a250089d6001d3bb5cafd323540c757c1f4
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-11-13 13:02:16 +0330

    add create  permission class and add for each view

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 from .models import MyUser
 
+
+UserAdmin.list_display += ('is_author',)
+UserAdmin.fieldsets += (_("profile"), {"fields": ("is_author",)}),
 admin.site.register(MyUser, UserAdmin)
--- api/views.py
+++ api/views.py
@@ -1,20 +1,24 @@
 from rest_framework import generics
 from .serializers import PostSerializer,CategorySerializer
 from blog.models import Post,Category
+from .permissions import IsAuthorOrReadonly,IsSuperUser
 
 
 class PostList(generics.ListCreateAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostSerializer
+	permission_classes=[IsAuthorOrReadonly|IsSuperUser]
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostSerializer
+	permission_classes=[IsAuthorOrReadonly|IsSuperUser]
 
 class CategoryList(generics.ListCreateAPIView):
 	queryset = Category.objects.all()
 	serializer_class = CategorySerializer
+	permission_classes=[IsSuperUser]
 
 
 class CategoryPostList(generics.ListAPIView):
