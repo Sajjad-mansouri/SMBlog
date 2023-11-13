7bfb59a2ccacf7b19745aa082738baea30deaa9b
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-11-14 00:04:58 +0330

    add user base filtering to post

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from .serializers import PostSerializer,CategorySerializer
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from django.views.decorators.vary import vary_on_headers
+from django.db.models import Q
 from blog.models import Post,Category
 from .permissions import IsAuthorOrReadonly,IsSuperUser
 
@@ -16,6 +17,12 @@ class PostApiViewSet(viewsets.ModelViewSet):
 	def list(self, request, *args, **kwargs):
 		return super().list(request,*args,**kwargs)
 
+	def get_queryset(self):
+		if self.request.user.is_anonymous:
+			return self.queryset.filter(status='p')
+		if self.request.user.is_superuser:
+			return self.queryset
+		return self.queryset.filter(Q(status='p')|Q(author=self.request.user))
 
 class CategoryList(generics.ListCreateAPIView):
 	queryset = Category.objects.all()
