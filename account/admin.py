f5fe5fa8cb0d9160cfb375eeb2ad05752010fb28
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-11-13 21:37:17 +0330

    add is_author to usercreation admin

--- account/admin.py
+++ account/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
 from .models import MyUser
+from .forms import CustomUserCreationForm
 
 
 UserAdmin.list_display += ('is_author',)
 UserAdmin.fieldsets += (_("profile"), {"fields": ("is_author",)}),
-admin.site.register(MyUser, UserAdmin)
+UserAdmin.add_fieldsets += (None, {"fields": ("is_author",)}),
+
+admin.site.register(MyUser,UserAdmin)
