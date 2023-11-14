428655e3992d661fb51ff14fa9d499d1da7bde53
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-11-14 21:51:24 +0330

    create category post list

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 app_name='blog'
 urlpatterns=[
-	path('',views.Index.as_view(),name='index')
+	path('',views.Index.as_view(),name='index'),
+	path('post/<int:pk>/',views.PostDetailView.as_view(),name='post_detail'),
+	path('category/<slug:cat>/',views.CategoryPostList.as_view(),name='category_post_list'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from .models import Post,Category
 
 
@@ -11,3 +11,21 @@ class Index(ListView):
 	def get_queryset(self):
 		return Post.objects.filter(status='p')
 
+
+class PostDetailView(DetailView):
+	template_name='blog/post_detail.html'
+	queryset=Post.objects.filter(status='p')
+
+
+class CategoryPostList(ListView):
+	template_name='blog/index.html'
+	context_object_name='posts'
+
+	def get_queryset(self):
+		cat_slug=self.kwargs.get('cat')
+		self.category=Category.objects.get(slug=cat_slug)
+		return self.category.articles.filter(status='p')
+	def get_context_data(self,**kwargs):
+		kwargs=super().get_context_data(**kwargs)
+		kwargs['category']=self.category
+		return kwargs
