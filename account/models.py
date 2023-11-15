2e6b48bab9946f90217f4c806cb39f284bb2dfc8
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-11-15 17:38:17 +0330

    change email to be unique

--- account/models.py
+++ account/models.py
@@ -2,5 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class MyUser(AbstractUser):
+	email=models.EmailField(max_length=200,unique=True)
 	is_author=models.BooleanField(default=False)
 
